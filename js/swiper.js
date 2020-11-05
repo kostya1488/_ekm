7cc7a866b2bca0f0bbbe74df982326f01cf6b82d
kostya <matvienkoigor956@gmail.com>
2020-11-05 18:49:11 +0200

    finished mobile version

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+if (screen.width < 1200) {
+    var promoSwiper = new Swiper('.promo_swiper', {
+        spaceBetween: 10,
+
+    });
+}
+var swiper = new Swiper('.about_swiper', {
+    loop: true,
+    spaceBetween: 30,
+    speed: 2800,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1000,
+    },
+});
