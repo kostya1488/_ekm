d388224398e2a1047eaba93af56e1acf4246cb25
kostya <matvienkoigor956@gmail.com>
2020-11-06 13:24:36 +0200

    changed content

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
+// toggle menu button
 var menuButton = document.getElementById('menu_button');
 menuButton.onclick = () => document.body.classList.toggle('menu_active');
+
+var formButton = document.getElementById('form_button');
+
+var formWrap = document.querySelector('.form_wrap');
+
+formButton.onclick = () => {
+
+    formWrap.classList.add('form_send');
+    // return false
+}
--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,9 @@
 if (screen.width < 1200) {
     var promoSwiper = new Swiper('.promo_swiper', {
         spaceBetween: 10,
-
+        pagination: {
+            el: '.swiper-pagination',
+        },
     });
 }
 var swiper = new Swiper('.about_swiper', {
