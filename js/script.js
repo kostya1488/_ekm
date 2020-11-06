30e6109bf8b4703b85ce7037839e90613dbab030
kostya <matvienkoigor956@gmail.com>
2020-11-06 14:21:29 +0200

    edited content

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 // toggle menu button
 var menuButton = document.getElementById('menu_button');
 menuButton.onclick = () => document.body.classList.toggle('menu_active');
+// ------------------
 
 var formButton = document.getElementById('form_button');
-
 var formWrap = document.querySelector('.form_wrap');
 
 formButton.onclick = () => {
-
     formWrap.classList.add('form_send');
-    // return false
+    return false
 }
+
+var navLink = document.querySelectorAll('.nav_link');
+
+navLink.forEach(element => {
+    element.onclick = () => {
+        document.body.classList.remove('menu_active');
+    }
+
+});
